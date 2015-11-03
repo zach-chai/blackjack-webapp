3d48ea442b51bd6b6f318a4ebef3e26d5b10a671
Zachary Chai <zachary.chai@outlook.com>
2015-11-02 22:26:17 -0500

    create new game if not set

--- app/controllers/blackjack_controller.rb
+++ app/controllers/blackjack_controller.rb
@@ -69,7 +69,7 @@ class BlackjackController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_game
-      @game = Game.find(params[:game_id])
+      @game = Game.find_by_id(params[:game_id]) || Game.create
     end
 
     def set_player
